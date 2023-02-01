85c1fbc7d803c272ae0d4720dab1708d81b3df99
Marius Aiordachioaei <aiordachioaei.marius@gmail.com>
2023-02-01 22:27:10 +0200

    Fix error messages

--- src/main/kotlin/parser/UnexpectedParsingError.kt
+++ src/main/kotlin/parser/UnexpectedParsingError.kt
@@ -5,4 +5,4 @@ import org.antlr.v4.kotlinruntime.ParserRuleContext
 class UnexpectedParsingError(
     message: String,
     ctx: ParserRuleContext
-) : ParsingError("$message. This does not indicate a problem with your code, but a problem with a parser", ctx)
+) : ParsingError("$message. This does not indicate a problem with your code, but a problem with the parser", ctx)
--- src/main/kotlin/parser/Utils.kt
+++ src/main/kotlin/parser/Utils.kt
@@ -3,7 +3,7 @@ package parser
 import org.antlr.v4.kotlinruntime.ParserRuleContext
 
 internal fun <T : ParserRuleContext, R> T.assertRequire(getter: T.() -> R?): R =
-    getter(this) ?: throw UnexpectedParsingError("Error", this)
+    getter(this) ?: throw UnexpectedParsingError("Failed to parse", this)
 
 internal fun ParserRuleContext.throwUnexpected(message: String): Nothing =
     throw UnexpectedParsingError(message, this)
